2db4a3e6cce08898792a625415252efbadc9a2b7
bansi <kiyadabansi@gmail.com>
2023-02-06 20:17:34 +0530

    add a modified files

--- app.js
+++ app.js
@@ -125,3 +125,11 @@ function onCalc() {
             break;
     }
 }
+
+for(var i=0;i<=5;i++){
+    
+    for(var j=0;j<=i;j++){
+        document.write("*");
+    }
+    document.write("<br>");
+}
