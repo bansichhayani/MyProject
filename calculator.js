585d58ef94ffe42f4b28ebc515b8fdac01f70502
bansi <kiyadabansi@gmail.com>
2023-02-27 18:27:03 +0530

    Add a modified files

--- calculator.js
+++ calculator.js
@@ -4,10 +4,40 @@
 function calculateLoan(){
     let LoanAmount = document.getElementById('lamount').value;
     let InteretRate = document.getElementById('irate').value;
-    let time = document.getElementById('mpay').value;
+    let time = document.getElementById('tpay').value;
 
     let interest = (LoanAmount * InteretRate * time)/100;
-    console.log(interest);
+    
     let total = parseInt(LoanAmount) + parseInt(interest);
-    console.log(total);
+    
+    document.getElementById('mpay').value = total;
+}
+
+//BMI calculator
+
+function calculateBMI(){
+    let Height = document.getElementById('height').value;
+    let Weight = document.getElementById('weight').value;
+
+    let Feet = Height * 0.3048;
+   
+    let BMI = Weight / (Feet * Feet);
+    
+    document.getElementById('bmi').value = BMI;
+}
+
+//Weight Converter
+
+function calculateWeight(){
+    let Pound = document.getElementById('pound').value;
+
+    let Kg = Pound / 2.2046;
+    
+    document.getElementById('weig').value = Kg;
+}
+
+//Digital Calculator
+
+function Zero(){
+    document.getElementById('ans').vlaue = 0;
 }
